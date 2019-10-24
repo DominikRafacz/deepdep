b72fef928fad36a23f43b9e4ba843732d149a7b2
Dominik Rafacz <d.rafacz@student.mini.pw.edu.pl>
2019-10-24 16:03:01 +0200

    added basic function with null implementation

---
+++ R/deepdep.R
@@ -0,0 +1,17 @@
+#' Visualizing dependiences between packages 
+#' 
+#' @description Visualize connections between packages. 
+#' 
+#' @param package \code{\link{Character}} vector giving a package name.
+#' @param plot_type \code{\link{Character}} string giving the type of the plot - either "circular",
+#' "horizontal tree" or "vertical tree".
+#' @param deps_depth \code{\link{Character}} string or \code{\link{numeric}} vector, giving 
+#' information what types of dependencies to plot and how deep - see details.
+#' 
+#' @export
+#' 
+deepdep <- function(package, 
+                    plot_type = "circular",
+                    deps_depth = "standard") {
+  NULL
+}
