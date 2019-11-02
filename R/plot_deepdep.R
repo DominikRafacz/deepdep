a66a1836550b9c07f04a0c27bdb78c3b2af1521e
MatiFilozof <matibakala@gmail.com>
2019-11-02 19:01:40 +0100

    plot fixed

--- R/plot_deepdep.R
+++ R/plot_deepdep.R
@@ -25,6 +25,7 @@
 #' @importFrom ggforce geom_circle
 #' @importFrom igraph V
 #' @importFrom igraph graph_from_data_frame
+#' @importFrom graphlayouts draw_circle
 #' @import ggraph
 #' @export
 plot.deepdep <- function(x, plot_type, same_level = FALSE, n_iter = 10, ...) {
@@ -39,18 +40,12 @@ plot.deepdep <- function(x, plot_type, same_level = FALSE, n_iter = 10, ...) {
   
   switch (plot_type,
     circular = {
-      G_layout <- create_layout(graph = G, layout = "tree", circular = TRUE)
-      for (i in 1:n_iter) {
-        G_layout <- gravity_iteration(layout = G_layout, G = G)
-      }
-      g <- ggraph(graph = G, layout = "manual", node.position = G_layout) +
-        geom_circle(aes(x0 = 0, y0 = 0, r = (layer-1)/max(layer-1), colour = factor(layer)), size = 3) +
+      g <- ggraph(graph = G, layout = "focus", focus = 1) +
+        draw_circle(use = "focus", max.circle = max(V(G)$layer - 1)) +
         geom_edge_link(aes(colour = type), arrow = arrow(angle = 16.6, ends = "first", type = "closed")) +
-        geom_node_point(aes(colour = factor(layer)), size = 5) +
+        geom_node_point(aes(fill = as.factor(layer)), size = 2, shape = 21) +
         geom_node_label(aes(label = names(V(G)), fill = factor(layer))) +
-        xlim(c(-1, 1)) +
-        ylim(c(-1, 1)) +
-        coord_equal() +
+        coord_fixed() +
         theme_void()
     },
     tree = {
