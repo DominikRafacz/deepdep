f8c0f13706b65daab28103355dd65cc3f378b28d
Dominik Rafacz <d.rafacz@student.mini.pw.edu.pl>
2019-11-08 21:18:14 +0100

    basic tests for plots

---
+++ tests/testthat/test_plot.R
@@ -0,0 +1,18 @@
+context("Tests for plots")
+
+deps <- deepdep("ttdo", depth = 2)
+
+
+test_that("dependencies plot have suitable layers and objects", {
+  plt <- plot_dependencies(deps)
+  expect_length(plt$layers, 4)
+  expect_s3_class(plt$layers[[1]]$geom, "GeomPath")
+  expect_s3_class(plt$layers[[1]]$stat, "StatIdentity")
+  expect_s3_class(plt$layers[[2]]$geom, "GeomEdgePath")
+  expect_s3_class(plt$layers[[2]]$stat, "StatEdgeLink")
+  expect_s3_class(plt$layers[[3]]$geom, "GeomPoint")
+  expect_s3_class(plt$layers[[3]]$stat, "StatFilter")
+  expect_s3_class(plt$layers[[4]]$geom, "GeomLabel")
+  expect_s3_class(plt$layers[[4]]$stat, "StatFilter")
+  
+})
