a10509a6682829a7f68c6f89dc9e645a53510e22
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-03-24 00:54:07 +0100

    fixed preparing prebuilt vignettes

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,6 +1,17 @@
 library(knitr)
-knit("vignettes/deepdep-comparison.Rmd.orig", "vignettes/deepdep-comparison.Rmd")
-knit("vignettes/deepdep-package.Rmd.orig", "vignettes/deepdep-package.Rmd")
-file.remove(list.files("vignettes/figure", full.names = TRUE))
-file.remove("vignettes/figure")
-file.rename("figure", "vignettes/figure")
+all_vignette_files <- list.files("vignettes", full.names = TRUE)
+vignettes_original_files <- all_vignette_files[grep("\\.orig", all_vignette_files)]
+for (filename in vignettes_original_files) {
+  newname <- sub("\\.orig", "", filename)
+  #knit(filename, newname)
+  content <- readLines(newname)
+  writeLines(gsub('(?<=<img src=\\")figure/(.*)(?=\\.png)', "\\1", content, perl=TRUE), newname)
+}
+  knit(filename, sub("\\.orig", "", filename))
+
+file.remove(all_vignette_files[grep("\\.png", all_vignette_files)])
+all_figure_files <- list.files("figure", full.names = TRUE)
+file.rename(all_figure_files, sub("figure", "vignettes", all_figure_files))
+file.remove("figure")
+
+for (filename in vignettes_original_files)
