72b1adb05220e3ae9c6fde4b4fbc1ae0c3b3dd49
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-03-31 23:28:14 +0200

    fixed precompile.R, updated vignettes

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,17 +1,15 @@
 library(knitr)
 all_vignette_files <- list.files("vignettes", full.names = TRUE)
 vignettes_original_files <- all_vignette_files[grep("\\.orig", all_vignette_files)]
+
 for (filename in vignettes_original_files) {
   newname <- sub("\\.orig", "", filename)
-  #knit(filename, newname)
+  knit(filename, newname)
   content <- readLines(newname)
   writeLines(gsub('(?<=<img src=\\")figure/(.*)(?=\\.png)', "\\1", content, perl=TRUE), newname)
 }
-  knit(filename, sub("\\.orig", "", filename))
 
 file.remove(all_vignette_files[grep("\\.png", all_vignette_files)])
 all_figure_files <- list.files("figure", full.names = TRUE)
 file.rename(all_figure_files, sub("figure", "vignettes", all_figure_files))
 file.remove("figure")
-
-for (filename in vignettes_original_files)
