79e038a778f8704dc0faab7164c3c33801167473
Laura <erdaradun.gaztea@gmail.com>
2021-12-10 00:00:34 +0100

    remove now unused functions

--- R/api_utils.R
+++ R/api_utils.R
@@ -1,39 +1,3 @@
-#' @title Get the description file from CRAN Data Base
-#'
-#' @description [CRAN Data Base](http://crandb.r-pkg.org)
-#'
-#' @param api package name
-#' @param head length of head lines
-#' @param tail length of tail lines
-#'
-#' @noRd
-DB <- function(api, head = 1e6, tail = head) {
-  ret <- paste0("http://crandb.r-pkg.org", "/", api)
-  ret <- httr::GET(ret)
-  ret <- httr::content(ret, as = "text", encoding = "UTF-8")
-  ret <- jsonlite::prettify(ret)
-  ret <- skip_lines(ret, head = head, tail = tail)
-
-  ret
-}
-
-skip_lines <- function(text, head = 1e6, tail = 1e6) {
-
-  text <- strsplit(text, "\n")[[1]]
-
-  tail <- min(tail, max(0, length(text) - head))
-
-  skip_text <- if (length(text) > head + tail) {
-    paste("\n... not showing", length(text) - head - tail, "lines ...\n")
-  } else {
-    character()
-  }
-
-  ret <- c(head(text, head), skip_text, tail(text, tail))
-  ret <- paste(ret, collapse = "\n")
-  ret
-}
-
 #' @title Check if the package name is valid
 #'
 #' @param package package name
