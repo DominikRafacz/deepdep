d7cd2583f302d27d4c5cdf400d8ce0d63cb0fd13
Dirk Eddelbuettel <edd@debian.org>
2021-12-10 08:01:27 -0600

    Allow tests to run when vcr is not installed

--- tests/testthat/test-deepdep.R
+++ tests/testthat/test-deepdep.R
@@ -22,16 +22,18 @@ test_that("using shorthand parameters returns the same result", {
   expect_identical(dd, dd2)
 })
 
-reset_cached_files("ava")
-reset_cached_files("deps")
-reset_cached_files("desc")
+if (requireNamespace("vcr", quietly=TRUE)) {
+  reset_cached_files("ava")
+  reset_cached_files("deps")
+  reset_cached_files("desc")
 
-vcr::use_cassette("deepdep-1", {
-  test_that("obtaining Bioc dependencies returns objects of correct types", {
-    dd <- deepdep_wrapped("les", downloads = FALSE, bioc = TRUE, depth = 1, dependency_type = c("Depends", "Imports", "Enhances", "LinkingTo"))
-    expect_is(dd, "deepdep")
+  vcr::use_cassette("deepdep-1", {
+    test_that("obtaining Bioc dependencies returns objects of correct types", {
+      dd <- deepdep_wrapped("les", downloads = FALSE, bioc = TRUE, depth = 1, dependency_type = c("Depends", "Imports", "Enhances", "LinkingTo"))
+      expect_is(dd, "deepdep")
+    })
   })
-})
+}
 
 
 test_that("incorrect combination of parameters results in error",{
@@ -41,13 +43,15 @@ test_that("incorrect combination of parameters results in error",{
   expect_error(deepdep("ggforce", downloads = TRUE, local =  TRUE, bioc = FALSE, depth = 2, dependency_type = c("Depends", "Imports")))
 })
 
-reset_cached_files("ava")
-reset_cached_files("deps")
-reset_cached_files("desc")
+if (requireNamespace("vcr", quietly=TRUE)) {
+  reset_cached_files("ava")
+  reset_cached_files("deps")
+  reset_cached_files("desc")
 
-vcr::use_cassette("deepdep-2", {
-  test_that("packages with no dependencies return empty deepdep object in result", {
-    dd <- deepdep("rlang")
-    expect_equal(nrow(dd), 0)
+  vcr::use_cassette("deepdep-2", {
+    test_that("packages with no dependencies return empty deepdep object in result", {
+      dd <- deepdep("rlang")
+      expect_equal(nrow(dd), 0)
+    })
   })
-})
+}
