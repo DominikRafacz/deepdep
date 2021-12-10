206f31d90d39fed5109debf17ff7f934bf495dbd
Laura <erdaradun.gaztea@gmail.com>
2021-12-11 00:00:59 +0100

    rewrite tests to skip if vcr not installed

--- tests/testthat/test-deepdep.R
+++ tests/testthat/test-deepdep.R
@@ -22,19 +22,18 @@ test_that("using shorthand parameters returns the same result", {
   expect_identical(dd, dd2)
 })
 
-if (requireNamespace("vcr", quietly=TRUE)) {
+test_that("obtaining Bioc dependencies returns objects of correct types", {
+  skip_if_not_installed("vcr")
+  
   reset_cached_files("ava")
   reset_cached_files("deps")
   reset_cached_files("desc")
-
+  
   vcr::use_cassette("deepdep-1", {
-    test_that("obtaining Bioc dependencies returns objects of correct types", {
-      dd <- deepdep_wrapped("les", downloads = FALSE, bioc = TRUE, depth = 1, dependency_type = c("Depends", "Imports", "Enhances", "LinkingTo"))
-      expect_is(dd, "deepdep")
-    })
+    dd <- deepdep_wrapped("les", downloads = FALSE, bioc = TRUE, depth = 1, dependency_type = c("Depends", "Imports", "Enhances", "LinkingTo"))
+    expect_is(dd, "deepdep")
   })
-}
-
+})
 
 test_that("incorrect combination of parameters results in error",{
   expect_error(deepdep_wrapped("ggforce", downloads = TRUE, local =  FALSE, bioc = TRUE, depth = 2, dependency_type = c("Depends", "Imports")))
@@ -43,15 +42,15 @@ test_that("incorrect combination of parameters results in error",{
   expect_error(deepdep("ggforce", downloads = TRUE, local =  TRUE, bioc = FALSE, depth = 2, dependency_type = c("Depends", "Imports")))
 })
 
-if (requireNamespace("vcr", quietly=TRUE)) {
+test_that("packages with no dependencies return empty deepdep object in result", {
+  skip_if_not_installed("vcr")
+  
   reset_cached_files("ava")
   reset_cached_files("deps")
   reset_cached_files("desc")
-
+  
   vcr::use_cassette("deepdep-2", {
-    test_that("packages with no dependencies return empty deepdep object in result", {
-      dd <- deepdep("rlang")
-      expect_equal(nrow(dd), 0)
-    })
+    dd <- deepdep("rlang")
+    expect_equal(nrow(dd), 0)
   })
-}
+})
