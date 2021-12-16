ca5a8611b8dddebdbbae41f9f58ea1a724cc8a61
Laura <erdaradun.gaztea@gmail.com>
2021-12-16 23:53:30 +0100

    reimplement require packages

--- R/require_packages.R
+++ R/require_packages.R
@@ -1,44 +1,44 @@
+require_packages <- function(packages, use_case = "access some functions") {
+  installed <- vapply(packages, require_pkg, FUN.VALUE = logical(1),
+                      use_case = use_case)
+  if (!all(installed)) {
+    packages <- deparse(packages[!installed])
+    message("To ", use_case, ", install dependencies with install.packages(", packages, ").")
+  }
+  all(installed)
+}
+
+require_pkg <- function(pkg, use_case = "access some functions") {
+  if (!is_pkg_installed(pkg) && interactive()) {
+    install_prompt(pkg, use_case = use_case)
+  }
+  # Return TRUE if installation successful
+  is_pkg_installed(pkg)
+}
+
 is_pkg_installed <- function(pkg) {
   check_pkg <- try(find.package(pkg), silent = TRUE)
   !inherits(check_pkg, "try-error")
 }
 
-require_pkg <- function(pkg, use_case = "access some functions") {
-  if (interactive()) {
-    if (!is_pkg_installed(pkg) &&
-        !getOption(paste0(pkg, "_suppress_prompt"), default = FALSE)) {
-      response <- menu(
-        c("yes", "no", "no and don't ask me anymore"),
-        title = paste0("To access some functionalities, you should install '", pkg, "' available on CRAN. Install?")
-      )
-      switch(response,
-             tryCatch(install.packages(pkg),
-                      finally = {
-                        if (!is_pkg_installed(pkg))
-                          warning("There was an error during an attempt to install '", pkg, "'.", call. = FALSE)
-                      }),
-             message("You cannot access full functionality of this package without having installed '", pkg, "' first. ",
-                     "You can do it manually by calling install.packages('", pkg, "')."),
-             {
-               options(setNames(list(TRUE), paste0(pkg, "_suppress_prompt")))
-               cat("Ok, but you cannot access full functionality of this package without having installed '", pkg, "' first.",
-                   sep = "")
-             },
-             message("You cannot access full functionality of this package without having installed '", pkg, "' first. ",
-                     "You can do it manually by calling install.packages('", pkg, "')."))
-    }
-  } else {
-    message("To ", use_case, ", install '", pkg, "' with install.packages('", pkg, "').")
-  }
-}
-
-require_packages <- function(packages, use_case = "access some functions") {
-  if (interactive()) {
-    for (pkg in packages) {
-      require_pkg(pkg)
-    }
-  } else {
-    packages <- deparse(packages)
-    message("To ", use_case, ", install dependencies with install.packages(", packages, ").")
+install_prompt <- function(pkg, use_case = "access some functions") {
+  if (!getOption(paste0(pkg, "_suppress_prompt"), default = FALSE)) {
+    response <- menu(
+      c("yes", "no", "no and don't ask me anymore"),
+      title = paste0("To ", use_case, ", you should install '", pkg, "' available on CRAN. Install?")
+    )
+    switch(response,
+           tryCatch(install.packages(pkg),
+                    finally = {
+                      if (!is_pkg_installed(pkg))
+                        warning("There was an error during an attempt to install '", pkg, "'.", call. = FALSE)
+                    }),
+           message("You cannot access full functionality of this package without having installed '", pkg, "' first. "),
+           {
+             options(setNames(list(TRUE), paste0(pkg, "_suppress_prompt")))
+             cat("Ok, but you cannot access full functionality of this package without having installed '", pkg, "' first.",
+                 sep = "")
+           },
+           message("You cannot access full functionality of this package without having installed '", pkg, "' first. "))
   }
 }
