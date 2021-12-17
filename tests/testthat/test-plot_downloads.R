1ef7c5c70ef9a0b4450cff812c3e2d7da02604e7
Laura <erdaradun.gaztea@gmail.com>
2021-12-17 12:16:43 +0100

    remove one cassette due to vcr URL encoding issue
    Related issue: https://github.com/ropensci/vcr/issues/240

--- tests/testthat/test-plot_downloads.R
+++ tests/testthat/test-plot_downloads.R
@@ -5,10 +5,16 @@ test_that("dependencies plot has correct classes and attributes", {
   reset_cached_files("deps")
   reset_cached_files("desc")
   
-  vcr::use_cassette("plot-dnl-1", {
+  vcr::use_cassette("deepdep-4", {
     deps <- deepdep("xgboost", dependency_type = "Imports")
-    plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
   })
+  
+  # TODO: Use cassette again when this issue is solved:
+  #  https://github.com/ropensci/vcr/issues/240
+  # vcr::use_cassette("plot-dnl-1", {
+  #   plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
+  # })
+  plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
   
   # Is returned value a ggplot2 plot?
   expect_s3_class(plt, c("gg", "ggplot"))
