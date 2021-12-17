5ed8eee16ed5a7581b7f6aa050de7307cbbbcd4b
Laura <erdaradun.gaztea@gmail.com>
2021-12-17 12:32:14 +0100

    skip test instead of having HTTP query outside of the cassette

--- tests/testthat/test-plot_downloads.R
+++ tests/testthat/test-plot_downloads.R
@@ -5,16 +5,14 @@ test_that("dependencies plot has correct classes and attributes", {
   reset_cached_files("deps")
   reset_cached_files("desc")
   
-  vcr::use_cassette("deepdep-4", {
-    deps <- deepdep("xgboost", dependency_type = "Imports")
-  })
+  skip("Cassette unusable due to encoding issue in vcr (check vcr issue #240).")
   
   # TODO: Use cassette again when this issue is solved:
   #  https://github.com/ropensci/vcr/issues/240
-  # vcr::use_cassette("plot-dnl-1", {
-  #   plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
-  # })
-  plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
+  vcr::use_cassette("plot-dnl-1", {
+    deps <- deepdep("xgboost", dependency_type = "Imports")
+    plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
+  })
   
   # Is returned value a ggplot2 plot?
   expect_s3_class(plt, c("gg", "ggplot"))
