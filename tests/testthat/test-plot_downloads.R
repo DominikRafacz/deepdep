403011e3fccc0a9b15fcf9681c6d085758be8918
Laura <erdaradun.gaztea@gmail.com>
2021-12-17 02:48:30 +0100

    set fixed date for plot downloads test

--- tests/testthat/test-plot_downloads.R
+++ tests/testthat/test-plot_downloads.R
@@ -6,10 +6,9 @@ test_that("dependencies plot has correct classes and attributes", {
   reset_cached_files("desc")
   
   vcr::use_cassette("plot-dnl-1", {
-    deps <- deepdep("xgboost", dependency_type = "Imports", downloads = TRUE)
+    deps <- deepdep("xgboost", dependency_type = "Imports")
+    plt <- plot_downloads(deps, from = as.Date("2020-07-01"), to = as.Date("2021-06-30"))
   })
-  
-  plt <- plot_downloads(deps)
   
   # Is returned value a ggplot2 plot?
   expect_s3_class(plt, c("gg", "ggplot"))
