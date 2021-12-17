f692d70ec433db0b2827cd56408ebc4df7291a83
Laura <erdaradun.gaztea@gmail.com>
2021-12-18 00:17:00 +0100

    restructure deepdep plot tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -5,59 +5,61 @@ test_that("incorrect object type results in an error", {
 # Tests below make HTTP calls, so we use vcr to record them
 skip_if_not_installed("vcr")
 
-test_that("dependencies plot has correct layers and objects", {
-  reset_cached_files("ava")
-  reset_cached_files("deps")
-  reset_cached_files("desc")
-  
-  vcr::use_cassette("plot-1", {
-    deps <- deepdep("shiny", depth = 2)
-  })
-  
-  plt <- plot_dependencies(deps)
-  expect_length(plt$layers, 4)
-  expect_s3_class(plt$layers[[1]]$geom, "GeomPath")
-  expect_s3_class(plt$layers[[1]]$stat, "StatIdentity")
-  expect_s3_class(plt$layers[[2]]$geom, "GeomEdgePath")
-  expect_s3_class(plt$layers[[2]]$stat, "StatEdgeLink")
-  expect_s3_class(plt$layers[[3]]$geom, "GeomPoint")
-  expect_s3_class(plt$layers[[3]]$stat, "StatFilter")
-  expect_s3_class(plt$layers[[4]]$geom, "GeomLabel")
-  expect_s3_class(plt$layers[[4]]$stat, "StatFilter")
+reset_cached_files("ava")
+reset_cached_files("deps")
+reset_cached_files("desc")
+
+# Write cassettes first
+vcr::use_cassette("plot-dd-shiny", {
+  dd_shiny <- deepdep("shiny", depth = 2)
+})
+vcr::use_cassette("plot-dd-rlang", {
+  dd_rlang <- deepdep("rlang")
+  plt_rlang <- plot_dependencies(dd_rlang)
+})
+vcr::use_cassette("plot-dd-datatable", {
+  dd_dt <- deepdep("data.table", depth = 2, dependency_type = "all")
 })
 
-test_that("plotting deepdep object with no rows results in less layers", {
-  reset_cached_files("ava")
-  reset_cached_files("deps")
-  reset_cached_files("desc")
+test_that("deepdep plot has correct layers and objects", {
+  plt_shiny <- plot_dependencies(dd_shiny)
   
-  vcr::use_cassette("plot-2", {
-    plt <- plot_dependencies("rlang")
-  })
-  
-  expect_s3_class(plt$layers[[1]]$geom, "GeomPoint")
-  expect_s3_class(plt$layers[[1]]$stat, "StatFilter")
-  expect_s3_class(plt$layers[[2]]$geom, "GeomLabel")
-  expect_s3_class(plt$layers[[2]]$stat, "StatFilter")
+  expect_length(plt_shiny$layers, 4)
+  # 1st layer is circles
+  expect_s3_class(plt_shiny$layers[[1]]$geom, "GeomPath")
+  expect_s3_class(plt_shiny$layers[[1]]$stat, "StatIdentity")
+  # 2nd layer is edges
+  expect_s3_class(plt_shiny$layers[[2]]$geom, "GeomEdgePath")
+  expect_s3_class(plt_shiny$layers[[2]]$stat, "StatEdgeLink")
+  # 3rd layer is node points
+  expect_s3_class(plt_shiny$layers[[3]]$geom, "GeomPoint")
+  expect_s3_class(plt_shiny$layers[[3]]$stat, "StatFilter")
+  # 4th layer is node labels
+  expect_s3_class(plt_shiny$layers[[4]]$geom, "GeomLabel")
+  expect_s3_class(plt_shiny$layers[[4]]$stat, "StatFilter")
 })
 
-test_that("decluttering removes all Suggests and Enhances packages from outer layers", {
-  reset_cached_files("ava")
-  reset_cached_files("deps")
-  reset_cached_files("desc")
+test_that("deepdep plot with no dependencies has only a subset of layers", {
+  # No circles, no edges
+  # 1st layer is node points
+  expect_s3_class(plt_rlang$layers[[1]]$geom, "GeomPoint")
+  expect_s3_class(plt_rlang$layers[[1]]$stat, "StatFilter")
+  # 2nd layer is node labels
+  expect_s3_class(plt_rlang$layers[[2]]$geom, "GeomLabel")
+  expect_s3_class(plt_rlang$layers[[2]]$stat, "StatFilter")
+})
+
+test_that("declutter removes all Suggests and Enhances packages from outer layers", {
+  plt_dt <- plot_dependencies(dd_dt, declutter = TRUE)
   
-  vcr::use_cassette("deepdep-3", {
-    dd <- deepdep("data.table", depth = 2, dependency_type = "all")
-  })
-  plt <- plot_dependencies(dd, depth = 2, declutter = TRUE,
-                           dependency_type = "all")
-  
-  filtered_dd <- dd[dd$origin_level == 0 | !dd$type %in% c("Suggests", "Enhances"), ]
+  # Node labels are equal to the names appearing in the filtered data
+  filtered_dd <- dd_dt[dd_dt$origin_level == 0 |
+                         !dd_dt$type %in% c("Suggests", "Enhances"), ]
   packages_plotted <- unique(c(filtered_dd$origin, filtered_dd$name))
+  expect_setequal(packages_plotted, plt_dt$data$name)
   
-  expect_setequal(packages_plotted, plt$data$name)
-  
-  edge_attrs <- igraph::edge_attr(attr(plt$data, "graph"))
+  # If edge has type Suggests or Enhances, it starts in the center
+  edge_attrs <- igraph::edge_attr(attr(plt_dt$data, "graph"))
   expect_true(
     all(edge_attrs$origin_level[edge_attrs$type %in% c("Suggests", "Enhances")] == 0)
   )
