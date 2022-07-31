f399abb398437c49f1d496b7fbf6e399dfb421cc
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-31 11:06:29 +0200

    reimplement deepdep

--- R/deepdep.R
+++ R/deepdep.R
@@ -1,105 +1,99 @@
-#' @title Acquire the dependencies of the package on any depth level
-#'
-#' @description This function is an ultimate wrapper for \code{\link{get_dependencies}}. It inherits all of the arguments and
-#' allows to recursively search for the dependencies at the higher level of \code{depth}.
-#'
-#' @param package A \code{character}. Name of the package that is on CRAN, Bioconductor repository or locally installed.
-#' See \code{bioc} and \code{local} arguments.
-#' @param depth An \code{integer}. Maximum depth level of the dependency. By default it's \code{1}.
-#' @param downloads A \code{logical}. If \code{TRUE} add dependency downloads data. By default it's \code{FALSE}.
-#' @param bioc A \code{logical} value. If \code{TRUE} the Bioconductor dependencies data will be taken from the
-#' Bioconductor repository. For this option to work properly, \code{BiocManager} package needs to be installed.
-#' @param local A \code{logical} value. If \code{TRUE} only data of locally installed packages will be used (without API usage).
-#' @param dependency_type A \code{character} vector. Types of the dependencies that should be sought, a subset of
-#' \code{c("Imports", "Depends", "LinkingTo", "Suggests", "Enhances")}. Other possibilities are: character string
-#' \code{"all"}, a shorthand for the whole vector; character string \code{"most"} for the same vector without \code{"Enhances"};
-#' character string \code{"strong"} (default) for the first three elements of that vector. Works analogously to
-#' \code{\link[tools]{package_dependencies}}.
-#'
-#' @return An object of \code{deepdep} class.
-#'
-#' @seealso \code{\link{get_dependencies}}
-#'
-#' @examples
-#'
-#' \donttest{
-#' library(deepdep)
-#'
-#' dd_downloads <- deepdep("ggplot2")
-#' head(dd_downloads)
-#'
-#' dd_2 <- deepdep("ggplot2", depth = 2, downloads = TRUE)
-#' plot_dependencies(dd_2, "circular")
-#'
-#' dd_local <- deepdep("deepdep", local = TRUE)
-#' plot_dependencies(dd_local)
-#' }
-#'
-#'
+#' @importFrom stats setNames
+#' @importFrom woodendesc wood_dependencies
 #' @export
-deepdep <- function(package, depth = 1, downloads = FALSE, bioc = FALSE, local = FALSE,
-                    dependency_type = "strong") {
-
-  check_package_name(package, bioc, local)
-
-  pkg_dep <- get_dependencies(package, downloads, bioc, local, dependency_type)
-
-  # check if there are any dependencies
-  if (length(pkg_dep$name) > 0) {
-    ret <- data.frame(origin = package, pkg_dep, origin_level = 0, dest_level = 1)
-  } else {
-    ret <- data.frame(origin = character(), pkg_dep, origin_level = numeric(), dest_level = numeric())
+deepdep <- function(package, depth = 1, type = "strong", version = NULL,
+                    repos = "cran") {
+  # TODO: replace default repos with c("cran", "core")
+  #  or use omit_core_packages = TRUE/FALSE that may append "core" to repos
+  
+  # TODO: test argument values
+  type <- match_dependency_type(type)
+  
+  package_levels <- setNames(0, package)
+  level <- 0
+  missing_pkgs <- character()
+  
+  deps <- data.frame(
+    package = character(),
+    dependency = character(),
+    version = character(),
+    type = character(),
+    origin_level = integer(),
+    target_level = integer()
+  )
+  
+  while (level < depth) {
+    pkgs_to_query <- names(package_levels)[package_levels == level]
+    # Remove R as a dependency
+    pkgs_to_query <- pkgs_to_query[pkgs_to_query != "R"]
+    
+    new_deps <- wood_dependencies(pkgs_to_query, repos = repos)
+    missing_pkgs <- update_missing(missing_pkgs, new_deps)
+    new_deps <- filter_dependencies(new_deps, type)
+    if (is_empty(new_deps)) break
+    
+    new_deps <- transform_dependencies(new_deps)
+    package_levels <- update_package_levels(package_levels, level, new_deps)
+    new_deps <- add_levels(new_deps, package_levels, level)
+    
+    deps <- rbind(deps, new_deps)
+    level <- level + 1
   }
-
-  if (depth > 1) {
-    already_subdeped <- package
-    to_subdep <- pkg_dep$name
-
-    for (level in 2:depth) {
-      added <- do.call(rbind, lapply(to_subdep, function(pkg_name) {
-        pkg_subdep <- get_dependencies(pkg_name, downloads, bioc, local, dependency_type)
-        if (length(pkg_subdep$name) > 0) {
-          upper_layer_origin <- ret[match(pkg_subdep$name, ret$origin), "origin_level"]
-          upper_layer_dest <- ret[match(pkg_subdep$name, ret$name), "dest_level"]
-          cb <- ifelse(is.na(upper_layer_origin),
-                       ifelse(is.na(upper_layer_dest), level,
-                              upper_layer_dest),
-                       upper_layer_origin)
-
-          data.frame(origin = pkg_name,
-                     pkg_subdep,
-                     origin_level = level - 1,
-                     dest_level = cb)
-        } else NULL
-      }))
-
-      already_subdeped <- union(already_subdeped, to_subdep)
-      to_subdep <- setdiff(added$name, already_subdeped)
-      ret <- rbind(ret, added)
-    }
+  
+  if (length(missing_pkgs) > 0) {
+    warning(
+      "Dependencies not found for: ",
+      paste0(missing_pkgs, collapse = ", ")
+    )
   }
-
-  attr(ret, "package_name") <- package
-  class(ret) <- c("deepdep", "data.frame")
-  ret
+  
+  structure(
+    deps,
+    package_name = package,
+    class = c("deepdep", "data.frame")
+  )
 }
 
-#' @title Print function for an object of \code{deepdep} class
-#'
-#' @param x An object of \code{deepdep} class.
-#' @param ... other
-#'
-#'
-#' @examples
-#' \donttest{
-#' library(deepdep)
-#'
-#' dd <- deepdep("stringr")
-#' dd
-#' }
-#'
-#' @rdname print.deepdep
-#' @export
-print.deepdep <- function(x, ...) {
-  print.data.frame(x)
+update_missing <- function(missing_pkgs, deps) {
+  c(missing_pkgs, names(deps)[vapply(deps, is.null, logical(1))])
+}
+
+filter_dependencies <- function(deps, type) {
+  # Remove NULL dependencies
+  deps <- deps[!vapply(deps, is.null, logical(1))]
+  # Filter out dependencies not in type
+  deps <- lapply(deps, function(dep) {
+    dep[dep[["type"]] %in% type, ]
+  })
+  # Remove empty dependencies
+  deps[vapply(deps, function(dep) nrow(dep) > 0, logical(1))]
+}
+
+transform_dependencies <- function(deps) {
+  # Add origin column
+  deps <- mapply(function(dep, name) {
+    data.frame(origin = name, dep)
+  }, deps, names(deps), SIMPLIFY = FALSE)
+  # Merge data frames
+  deps <- do.call(rbind, c(deps, make.row.names = FALSE))
+  # Rename columns
+  names(deps)[c(1, 2)] <- c("package", "dependency")
+  deps
+}
+
+update_package_levels <- function(package_levels, level, deps) {
+  new_pkgs <- setdiff(deps[["dependency"]], names(package_levels))
+  new_pkgs <- setNames(rep(level + 1, length(new_pkgs)), new_pkgs)
+  c(package_levels, new_pkgs)
+}
+
+add_levels <- function(deps, package_levels, level) {
+  deps[["origin_level"]] <- level
+  deps[["target_level"]] <- package_levels[deps[["dependency"]]]
+  deps
+}
+
+# TODO: implement as a method for wood_deps/wood_dep_list maybe?
+is_empty <- function(deps) {
+  all(vapply(deps, nrow, numeric(1)) == 0)
 }
