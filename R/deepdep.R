fa47561f76a6439d0dc18ce9295afb0f3d467ba5
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-28 19:06:44 +0100

    use newer woodendesc functions

--- R/deepdep.R
+++ R/deepdep.R
@@ -1,21 +1,23 @@
 #' @importFrom stats setNames
-#' @importFrom woodendesc wood_dependencies
+#' @importFrom woodendesc filter_dependencies squash wood_dependencies
 #' @export
 deepdep <- function(package, depth = 1, type = "strong", version = NULL,
-                    repos = "cran") {
-  # TODO: replace default repos with c("cran", "core")
-  #  or use omit_core_packages = TRUE/FALSE that may append "core" to repos
+                    repos = c("core", "cran"),
+                    # Backwards compatible parameters (use NULL or missing)
+                    downloads = FALSE, bioc = FALSE, local = FALSE,
+                    dependency_type = "strong") {
+  # TODO: implement handling version code
+  
+  # TODO: implement backwards compatibility
   
   # TODO: test argument values
-  type <- match_dependency_type(type)
-  
   package_levels <- setNames(0, package)
   level <- 0
   missing_pkgs <- character()
   
   deps <- data.frame(
+    origin = character(),
     package = character(),
-    dependency = character(),
     version = character(),
     type = character(),
     origin_level = integer(),
@@ -30,9 +32,10 @@ deepdep <- function(package, depth = 1, type = "strong", version = NULL,
     new_deps <- wood_dependencies(pkgs_to_query, repos = repos)
     missing_pkgs <- update_missing(missing_pkgs, new_deps)
     new_deps <- filter_dependencies(new_deps, type)
-    if (is_empty(new_deps)) break
+    new_deps <- squash(new_deps)
     
-    new_deps <- transform_dependencies(new_deps)
+    if (nrow(new_deps) == 0) break
+    
     package_levels <- update_package_levels(package_levels, level, new_deps)
     new_deps <- add_levels(new_deps, package_levels, level)
     
@@ -49,7 +52,8 @@ deepdep <- function(package, depth = 1, type = "strong", version = NULL,
   
   structure(
     deps,
-    package_name = package,
+    package = package,
+    version = version,
     class = c("deepdep", "data.frame")
   )
 }
@@ -58,42 +62,14 @@ update_missing <- function(missing_pkgs, deps) {
   c(missing_pkgs, names(deps)[vapply(deps, is.null, logical(1))])
 }
 
-filter_dependencies <- function(deps, type) {
-  # Remove NULL dependencies
-  deps <- deps[!vapply(deps, is.null, logical(1))]
-  # Filter out dependencies not in type
-  deps <- lapply(deps, function(dep) {
-    dep[dep[["type"]] %in% type, ]
-  })
-  # Remove empty dependencies
-  deps[vapply(deps, function(dep) nrow(dep) > 0, logical(1))]
-}
-
-transform_dependencies <- function(deps) {
-  # Add origin column
-  deps <- mapply(function(dep, name) {
-    data.frame(origin = name, dep)
-  }, deps, names(deps), SIMPLIFY = FALSE)
-  # Merge data frames
-  deps <- do.call(rbind, c(deps, make.row.names = FALSE))
-  # Rename columns
-  names(deps)[c(1, 2)] <- c("package", "dependency")
-  deps
-}
-
 update_package_levels <- function(package_levels, level, deps) {
-  new_pkgs <- setdiff(deps[["dependency"]], names(package_levels))
+  new_pkgs <- setdiff(deps[["package"]], names(package_levels))
   new_pkgs <- setNames(rep(level + 1, length(new_pkgs)), new_pkgs)
   c(package_levels, new_pkgs)
 }
 
 add_levels <- function(deps, package_levels, level) {
   deps[["origin_level"]] <- level
-  deps[["target_level"]] <- package_levels[deps[["dependency"]]]
+  deps[["target_level"]] <- package_levels[deps[["package"]]]
   deps
-}
-
-# TODO: implement as a method for wood_deps/wood_dep_list maybe?
-is_empty <- function(deps) {
-  all(vapply(deps, nrow, numeric(1)) == 0)
 }
