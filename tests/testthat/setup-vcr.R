104df39bcd612cab5bdfe18dda5779f121b76987
DominikRafacz <dominikrafacz@gmail.com>
2025-08-24 19:06:18 +0200

    fix issue with vcr2

--- tests/testthat/setup-vcr.R
+++ tests/testthat/setup-vcr.R
@@ -1,8 +1,6 @@
 if (requireNamespace("vcr", quietly = TRUE)) {
   library("vcr") # *Required* as vcr is set up on loading
   invisible(vcr::vcr_configure(
-    dir = vcr::vcr_test_path("fixtures"),
-    verbose_errors = TRUE
+    dir = vcr::vcr_test_path("fixtures")
   ))
-  vcr::check_cassette_names()
 }
